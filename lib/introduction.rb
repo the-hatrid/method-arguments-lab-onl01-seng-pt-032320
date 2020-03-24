acef8f2920fb076964f29db7260cc8a66f23a191
bruce willis <fashun_vyctum@yahoo.com>
2020-03-24 22:18:30 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,7 @@
-def introduction(Josh)
+def introduction("Josh")
   puts "Hi, my name is #{name}."
 end
 
-def introduction_with_language(Dan, Ember JS)
+def introduction_with_language("Dan", "Ember.JS")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
